21817ed6820a57bbdb355100a639c67eb7c5499e
Watliel <arnaud.salomon20@gmail.com>
2021-04-14 10:14:08 +0200

    adding exercises finished

--- exercises/02_conditions.asm
+++ exercises/02_conditions.asm
@@ -1,10 +1,10 @@
 # Exercise 2
 # Notions:
 #
-# - be able to make a conditional jump in a program here.
-# - test the equality between two registers.
-# - carry out a conditional then unconditional branching.
-# - then display a message according to the result.
+# Be able to make a conditional jump in a program here
+# test the equality between two registers.
+# Carry out a conditional then unconditional branching.
+# Then display a message according to the result.
 
 # Objective of the program:
 #
@@ -24,8 +24,7 @@ msg_neq: .asciz "These numbers are not equal"
 # ```rust
 # let a = 4;
 # let b = 5;
-# let msg = if a == b { "These numbers are equal!" }
-#                else { "These numbers are not equal" };
+# let msg = if a == b { "These numbers are equal!" } else { "These numbers are not equal" };
 # println!(msg);
 # ```
 
@@ -33,7 +32,7 @@ msg_neq: .asciz "These numbers are not equal"
 # two instructions to write to load 5 into t0 and t1 into 4.
 
 # start of the program.
-main:
+hand:
 li t0, 4
 li t1, 5
 
@@ -41,12 +40,12 @@ li t1, 5
 bne t0, t1, not_eq # if t0! = t1 jump not_eq
 
 # Equal cases t0 == t1
-la ???, ??? # msg <- "These numbers are equal!"
+the ???, ??? # msg <- "These numbers are equal!"
 j end # We are done so we can get out of the if.
 
 # Unequal cases t0 != T1
 not_eq:
-la ???, ??? # msg <- "These numbers are not equal"
+the ???, ??? # msg <- "These numbers are not equal"
 ###
 
 end:
@@ -54,8 +53,8 @@ end:
 # Display:
 #
 # Here we want to display on the console via a system call.
-# Take inspiration from 01_syscall, register a0 must contain the number of the
-# syscall printString: 4 and a7 the string to display.
+# take inspiration from 01_syscall, register a0 must contain
+# the number of the syscall printString: 4 and a7 the string to display.
 
 li ???, 4 # syscall number
 ???       # which instruction should we use to make a syscall?
