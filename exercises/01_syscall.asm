ff781880b77a0ae0a7844373f4028850b333b528
Watliel <arnaud.salomon20@gmail.com>
2021-04-14 11:02:46 +0200

    complete exo 2

--- exercises/01_syscall.asm
+++ exercises/01_syscall.asm
@@ -12,18 +12,30 @@
 
 # the .asciz directive allows you to declare a string ending with a 0x00, '\0' in ASCII.
 message: .asciz "Hello ASM from .data"
+# number: .double 666
 
 .text # <- code segment.
 
 # Exercise: Observation in RARS
 #
 # - What is the base address of the msg chain? His end address?
+# In Hexa the base address of the msg chain is OX10010000 and his end address is OX10010014
+# Else the base address of the msg chain is 268500992 and his end address is 268501012 
 # - In which segment of memory is it?
+# the segment of memory is GP
 # - What are the hexadecimal values ​​present in .data?
+# the hexadecimal values is 0x10008000
 
 # la: Load Address, load an address (label) into a register
-la a0, ??? # argument of the system call
-li a7, ???  # system call number 4 according to the RARS printString doc
+la a0, message # argument of the system call
+#la a1, number # argument of the system 
+li a7, 4  # system call number 4 according to the RARS printString doc
+
 ecall       # used to switch to kernel mode to make the system call
 
+
 # Bonus: Now display a number, you will need the documentation.
+li a0, 32 	# integer to print
+li a7, 1        
+ecall          # print it       
+
--- exercises/02_conditions.asm
+++ exercises/02_conditions.asm
@@ -1,10 +1,10 @@
 # Exercise 2
 # Notions:
 #
-# Be able to make a conditional jump in a program here
-# test the equality between two registers.
-# Carry out a conditional then unconditional branching.
-# Then display a message according to the result.
+# - be able to make a conditional jump in a program here.
+# - test the equality between two registers.
+# - carry out a conditional then unconditional branching.
+# - then display a message according to the result.
 
 # Objective of the program:
 #
@@ -24,7 +24,8 @@ msg_neq: .asciz "These numbers are not equal"
 # ```rust
 # let a = 4;
 # let b = 5;
-# let msg = if a == b { "These numbers are equal!" } else { "These numbers are not equal" };
+# let msg = if a == b { "These numbers are equal!" }
+#                else { "These numbers are not equal" };
 # println!(msg);
 # ```
 
@@ -32,7 +33,7 @@ msg_neq: .asciz "These numbers are not equal"
 # two instructions to write to load 5 into t0 and t1 into 4.
 
 # start of the program.
-hand:
+main:
 li t0, 4
 li t1, 5
 
@@ -40,12 +41,12 @@ li t1, 5
 bne t0, t1, not_eq # if t0! = t1 jump not_eq
 
 # Equal cases t0 == t1
-the ???, ??? # msg <- "These numbers are equal!"
+la a0, msg_eq # msg <- "These numbers are equal!"
 j end # We are done so we can get out of the if.
 
 # Unequal cases t0 != T1
 not_eq:
-the ???, ??? # msg <- "These numbers are not equal"
+la a0, msg_neq # msg <- "These numbers are not equal"
 ###
 
 end:
@@ -53,8 +54,8 @@ end:
 # Display:
 #
 # Here we want to display on the console via a system call.
-# take inspiration from 01_syscall, register a0 must contain
-# the number of the syscall printString: 4 and a7 the string to display.
+# Take inspiration from 01_syscall, register a0 must contain the number of the
+# syscall printString: 4 and a7 the string to display.
 
-li ???, 4 # syscall number
-???       # which instruction should we use to make a syscall?
+li a7, 4 # syscall number
+ecall       # which instruction should we use to make a syscall?
